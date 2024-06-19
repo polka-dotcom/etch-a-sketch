e68fd3d047c9748b5d958fe1c5ac1df1d5b03799
polka-dotcom <navyaonline7@gmail.com>
2024-06-19 20:10:07 +0400

    hover effect

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const container = document.querySelector('.container');
 for (let i=0; i< 16 * 16; i++) {
     const square = document.createElement('div'); 
     square.classList.add('square'); 
+  
+
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = 'black';
+    }); 
+
     container.appendChild(square);
 }
