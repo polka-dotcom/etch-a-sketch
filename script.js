9806d0d732787a287cf427f67dec9ead0ccdc9f0
polka-dotcom <navyaonline7@gmail.com>
2024-06-20 23:05:31 +0400

    added grid-size functionality

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const container = document.querySelector('.container'); 
+const gridBtn = document.getElementById('gridBtn');
+let gridSize= 16;
+const containerSize = 960;
 
-for (let i=0; i< 16 * 16; i++) {
-    const square = document.createElement('div'); 
-    square.classList.add('square'); 
-  
+function createGrid(num) {
+    container.innerHTML = ''; 
+    const squareSize = containerSize / num; 
 
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = 'black';
-    }); 
-
-    container.appendChild(square);
+    for (let i=0; i< num * num; i++) {
+        const square = document.createElement('div'); 
+        square.classList.add('square'); 
+        square.style.width = `${squareSize}px`; 
+        square.style.height = `${squareSize}px`; 
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = 'black';
+        }); 
+    
+        container.appendChild(square);
+    }
 }
+
+createGrid(gridSize); 
+
+gridBtn.addEventListener('click', () => {
+    gridSize = parseInt(prompt('Enter number of rows/columns: '));
+    while (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        gridSize = parseInt(prompt('Enter a number between 1 and 100: '));
+    }
+    createGrid(gridSize);
+});
